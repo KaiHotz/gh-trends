2f543faa3222dcf09fd4e4b7b14a0158085b8aa7
KaiHotz <kaihotz@me.com>
2021-07-06 16:44:20 +0400

    adjustment

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export const httpClient = Axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://gh-trending-api.herokuapp.com',
+  baseURL: 'https://gh-trending-api.herokuapp.com',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
